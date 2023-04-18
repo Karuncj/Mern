3231ffbb7a1acac16a91832aa86d680b189dc2cd
KARUN CHERY_JAMES <karunchery@gmail.com>
2023-04-18 22:01:29 +0530

    new file:   Backend/.env 	modified:   Backend/app.js

--- Backend/app.js
+++ Backend/app.js
@@ -1,10 +1,23 @@
 const express=require('express');
 const app=express();
+require('dotenv/config');
 
-app.get('/',(req,res)=>{
-    res.send('hello api')
+const api=process.env.API_URL;
+app.get(`${api}/products`,(req,res)=>{
+    const product={
+        id:1,
+        name:'dresser',
+        image:'some url'
+    }
+    res.send(product)
+})
+app.post(`${api}/products`,(req,res)=>{
+   const newProduct=req.body;
+   console.log(newProduct);
+    res.send(newProduct);
 })
 
 app.listen(3000,()=>{
+    console.log(api)
     console.log('server is running ');
 })
