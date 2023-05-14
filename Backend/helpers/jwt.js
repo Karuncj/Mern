c2e2314702ed0bf2a20af39b1e9d64c09df9fc03
KARUN CHERY_JAMES <karunchery@gmail.com>
2023-05-14 12:23:09 +0530

    modified:   Backend/helpers/jwt.js

--- Backend/helpers/jwt.js
+++ Backend/helpers/jwt.js
@@ -1,8 +1,8 @@
-const expressJwt = require('express-jwt');
+const {expressJwt:jwt} = require('express-jwt');
 
 function authJwt() {
     const secret = process.env.secret;
-    return expressJwt({
+    return jwt({
         secret,
         algorithms: ['HS256']
     });
